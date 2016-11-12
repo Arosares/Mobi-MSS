ca07f11f8f4868fc16d09aa43c04a5efe9dacaa2
Frank Keßler <frank-kessler@gmx.de>
2016-11-12 16:37:49 +0100

    Added Table functionality for adding cow; Not scrolling down yet; Not removing rows yet; Not looking good;

--- CowCounter/app/src/main/java/com/example/aro/cowcounter/MainActivity.java
+++ CowCounter/app/src/main/java/com/example/aro/cowcounter/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.aro.cowcounter;
 
+import android.content.Context;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
@@ -17,6 +19,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private List<Cow> cowList = new LinkedList<>();
+    private int rowCounter = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +34,11 @@ public class MainActivity extends AppCompatActivity {
         final TextView cowCounter = (TextView) findViewById(R.id.textView5);
         cowCounter.setText("Cows: " + cowList.size());
 
-        final TableRow tableRow = (TableRow) findViewById(R.id.tableRow);
+        final TableLayout tableLayout = (TableLayout) findViewById(R.id.tableLayout1);
+
+
+
+
 
         Button addButton = (Button) findViewById(R.id.button);
         addButton.setOnClickListener(new View.OnClickListener(){
@@ -48,6 +55,30 @@ public class MainActivity extends AppCompatActivity {
 
                     Cow cow = new Cow(breed, cowID);
                     cowList.add(cow);
+
+                    //Edit Table
+
+                    TableRow newRow = new TableRow(tableLayout.getContext());
+                    newRow.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT));
+
+                    TextView cowBreedTable = new TextView(newRow.getContext());
+
+                    cowBreedTable.setText(Integer.toString(cow.getBreed()));
+                    cowBreedTable.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT));
+
+                    TextView cowIDTable = new TextView(newRow.getContext());
+                    cowIDTable.setText(Integer.toString(cow.getCowID()));
+                    cowIDTable.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT));
+
+                    newRow.addView(cowBreedTable);
+                    newRow.addView(cowIDTable);
+                    newRow.setId(rowCounter);
+
+                    tableLayout.addView(newRow, new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT));
+
+
+
+                    //Update counter
                     cowCounter.setText("Cows: " + cowList.size());
                     System.out.println("Created Cow:");
                     System.out.println("Breed: " + cow.getBreed() + " ID: " + cow.getCowID());
